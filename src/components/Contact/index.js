c940247c9c800a675dbba7db9506a537ebd556ea
jonacko <jessonacko@gmail.com>
2022-12-02 17:41:59 +0000

    fixed warning in contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,45 +10,46 @@ const Contact = () => {
         e.preventDefault()
 
         emailjs.sendForm(
-            'service_wsuo1ip', 
-            'template_mc8b4er', refForm.current, 
+            'service_wsuo1ip',
+            'template_mc8b4er', refForm.current,
             '2m0czvZEu1ESUnjRU')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      })}
-    
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+    }
+
 
     return (
         <div className="container contact-page">
-        <div className="text-area">
-            <h1> Contact Me</h1>
-            <h2>Please feel free to contact me using the form below.</h2>
+            <div className="text-area">
+                <h1> Contact Me</h1>
+                <h2>Please feel free to contact me using the form below.</h2>
 
-            <div className="contact-form">
-                <form ref={refForm} onSubmit={sendEmail}>
-    <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
-                    <ul>
-                        <li className="half">
-                            <input type="text" name="user_name" placeholder="Name" required />
-                        </li>
-                        <li className="half">
-                            <input type="email" name="user_email" placeholder="Email" required />
-                        </li>
-                        <li>
-                            <input placeholder="Subject" type="text" name="subject" required />
-                        </li>
-                        <li><textarea placeholder="Message" name="message" required> </textarea>
-                        </li>
-                        <li>
-                            <input type="submit" className='flat-button' value="SEND" />
-                        </li>
+                <div className="contact-form">
+                    <form ref={refForm} onSubmit={sendEmail}>
+                        <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+                        <ul>
+                            <li className="half">
+                                <input type="text" name="user_name" placeholder="Name" required />
+                            </li>
+                            <li className="half">
+                                <input type="email" name="user_email" placeholder="Email" required />
+                            </li>
+                            <li>
+                                <input placeholder="Subject" type="text" name="subject" required />
+                            </li>
+                            <li><textarea placeholder="Message" name="message" required></textarea>
+                            </li>
+                            <li>
+                                <input type="submit" className='flat-button' value="SEND" />
+                            </li>
 
-                    </ul>
-                </form>
+                        </ul>
+                    </form>
+                </div>
             </div>
-        </div>
 
         </div>
     )
